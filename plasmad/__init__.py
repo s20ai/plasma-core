c08608ee628b61aec4a2e26dc53b29b41255cf8f
Tabish Imran <tabish.imran96@gmail.com>
2020-02-28 07:43:34 -0800

    Initial Commit

---
+++ plasmad/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for plasma-core."""
+
+__author__ = """Tabish Imran"""
+__email__ = 'vector@s20.ai'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Tabish Imran",
+    author_email='vector@s20.ai',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='plasma_core',
+    name='plasma_core',
+    packages=find_packages(include=['plasma_core', 'plasma_core.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/s20ai/plasma_core',
+    version='0.1.0',
+    zip_safe=False,
+)
