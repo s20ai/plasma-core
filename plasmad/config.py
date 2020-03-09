1f6e7472218b43d3f5920cfeb68b3d489a509e9f
Tabish Imran <tabish.imran96@gmail.com>
2020-03-08 23:57:23 -0700

    Adds Config for redis and mongo

--- plasmad/config.py
+++ plasmad/config.py
@@ -3,9 +3,14 @@ REST_API_HOST = '0.0.0.0'
 REST_API_PORT = 8196
 
 # MongoDB Config
+MONGODB_HOST = '127.0.0.1'
+MONGODB_PORT = 27017
+MONGODB_DB = 'plasma'
+MONGODB_PASSWD = None
 
-
-# Redis Config 
+# Redis Config
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT =  6379
 
 # WXE config
 EXECUTION_CONTEXT = 'celery'
