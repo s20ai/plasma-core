94dad46c6943ea40f804ba61d9f25e7fef4dd9cd
Tabish Imran <tabish.imran96@gmail.com>
2020-03-09 01:51:03 -0700

    Adds more detailed mongo config

--- plasmad/config.py
+++ plasmad/config.py
@@ -6,16 +6,24 @@ REST_API_PORT = 8196
 MONGODB_HOST = '127.0.0.1'
 MONGODB_PORT = 27017
 MONGODB_DB = 'plasma'
+MONGODB_AUTH = False
+MONGODB_USER = None
 MONGODB_PASSWD = None
+MONGODB_AUTH_DATABASE = None
+
+if MONGODB_AUTH is True:
+    MONGODB_CONNECTION_URI = 'mongodb://%s:%s@%s:%s/%s?authSource=%s' % (
+        MONGODB_USER, MONGODB_PASSWD, MONGODB_HOST, MONGODB_PORT, MONGODB_DB, MONGODB_AUTH_DATABASE)
+else:
+    MONGODB_CONNECTION_URI = 'mongodb://%s:%s/%s' % (
+        MONGODB_HOST, MONGODB_PORT, MONGODB_DB)
 
 # Redis Config
 REDIS_HOST = '127.0.0.1'
-REDIS_PORT =  6379
+REDIS_PORT = 6379
 
 # WXE config
 EXECUTION_CONTEXT = 'celery'
 
 # Logging Config
 LOG_PATH = 'plasma.log'
-
-
