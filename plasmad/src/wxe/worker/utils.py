f08742a5665a98fa0b06d83935b95bfd7a8c49fa
Tabish Imran <tabish.imran96@gmail.com>
2020-03-11 05:43:18 -0700

    Separates utility functions into a different file

---
+++ plasmad/src/wxe/worker/utils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def update_execution_job(execution_id, update):
+    url = 'http://0.0.0.0:8196/api/execution/'+execution_id
+    json = {'update': update}
+    response = requests.put(url, json=json)
+    if response.status_code == 204:
+        output = True
+    else:
+        output = False
+    return output
+
+
+def update_workflow(workflow_id, update):
+    url = 'http://0.0.0.0:8196/api/workflow/'+workflow_id
+    json = {'update': update}
+    response = requests.put(url, json=json)
+    if response.status_code == 204:
+        output = True
+    else:
+        output = False
+    return output
+
+
+def update_status(workflow_id, execution_id, status_code):
+    update_workflow(
+        workflow_id,
+        {"status": status_code}
+    )
+    update_execution_job(
+        execution_id,
+        {"status": status_code}
+    )
+
+
+def validate_job(execution_job):
+    update_status(
+        execution_job['workflow-id'],
+        execution_job['execution-id'],
+        2
+    )
